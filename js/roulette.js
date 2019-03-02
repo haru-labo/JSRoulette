5b80f75be52be3580fe2aa242b5e4eba9e416138
haru-labo <harukun.space@gmail.com>
2019-03-03 00:27:49 +0900

    add selecting process

--- js/roulette.js
+++ js/roulette.js
@@ -1,28 +1,64 @@
 let numOfBox = 16;
 let boxNum = 0;
+let idRulette;
+//ボックスの状態
+let $activeBox;
+let $selectingBox;
+let $selectedBox;
+let $inactiveBox;
 
-function addBlock() {
+function addBox() {
   for(let i = 0; i < numOfBox; i++) {
     boxNum++;
-    $('#outflame').append('<div></div>');
+    $('#outflame').append('<div class="active"></div>');
     $('#outflame :last-child').text(boxNum);
   }
 }
 
-function start() {
 
+//////////各ボタン処理//////////
+function start() {
+  idRulette = setInterval(roulette, 100);
 }
 
 function stop() {
-
+  clearInterval(idRulette);
+  $selectedBox = $('.selecting');
+  $selectedBox.removeClass('selecting');
+  $selectedBox.addClass('selected');
 }
 
 function reset() {
-
+  if(idRulette !== undefined) {
+    clearInterval(idRulette);
+  }
+  let $resetTarget = $('.inactive, .selecting, .selected');
+  $resetTarget.removeClass();
+  $resetTarget.addClass('active');
 }
 
+//////////ルーレット処理//////////
+function roulette() {
+  $activeBox = $('.active');
+  $selectingBox = $('.selecting');
+  $selectedBox = $('.selected');
+  let $activeBoxNum = $activeBox.length;
+  let selectingNum = Math.floor(Math.random() * $activeBoxNum);
+  let $targetBox = $activeBox.eq(selectingNum);
+  if ($selectedBox.length) {
+    $selectedBox.removeClass('selected');
+    $selectedBox.addClass('inactive');
+  }
+  $selectingBox.removeClass('selecting');
+  $selectingBox.addClass('active');
+  $targetBox.removeClass('active');
+  $targetBox.addClass('selecting');
+}
+
+
+
 $(function() {
-  addBlock();
+  addBox();
   $('#btnStart').click(start);
   $('#btnStop').click(stop);
   $('#btnReset').click(reset);
